52191e74955f6744e7ac1fcc257f1844d140d693
ram <khaled_17@agr.sohag.edu.eg>
2023-10-27 10:44:17 +0200

    html and css is done

---
+++ script.js
@@ -0,0 +1,52 @@
+
+// all words
+const words = [
+    {
+      word: "react",
+      disc: "JavaScript library",
+    },
+    {
+      word: "vue",
+      disc: "JavaScript Framework",
+    },
+    {
+      word: "angular",
+      disc: "JavaScript MVW Framework",
+    },
+    {
+      word: "nodejs",
+      disc: "JavaScript runtime environment",
+    },
+    {
+      word: "php",
+      disc: "general-purpose scripting language",
+    },
+    {
+      word: "ruby",
+      disc: "open source programming language",
+    },
+    {
+      word: "python",
+      disc: "Programming Language",
+    },
+    {
+      word: "tailwind",
+      disc: "A utility-first CSS framework",
+    },
+    {
+      word: "bootstrap",
+      disc: "world's most famous free CSS framework",
+    },
+  ];
+
+
+
+
+const inputsContener=document.querySelector(".inputs"),
+diskTitle=document.querySelector(".disk"),
+quessCount=document.querySelector(".guess_count"),
+resetBTn=document.querySelector('button'),
+typing=document.querySelector('.typing');
+
+
+
